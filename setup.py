68885571f3567376eb817d51c4258fbc113e0661
Nick Pratley <nick@ix.asn.au>
2020-03-11 21:57:18 +1100

    0.0.1 release

---
+++ setup.py
@@ -0,0 +1,40 @@
+"""
+pyexos - Extreme Networks Config Manipulation
+Copyright (C) 2020 Internet Association of Australian (IAA)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyexos-ixaustralia",
+    version="0.0.1",
+    author="Nick Pratley",
+    author_email="nick@ix.asn.au",
+    description="pyexos - An Extreme networks config manipulation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ixaustralia/pyexos",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha"
+        "Programming Language :: Python :: 3",
+        "License :: GPL version 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires="~=3.5",
+)
