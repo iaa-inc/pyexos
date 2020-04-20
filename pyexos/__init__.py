f7f94831862951d5162a94e0b6836e8b3ff2fd21
Nick Pratley <nick@ix.asn.au>
2020-04-20 18:56:43 +1000

    a

--- pyexos/__init__.py
+++ pyexos/__init__.py
@@ -29,4 +29,4 @@ __all__ = [
     NotImplementedException,
 ]
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -33,9 +33,11 @@ setuptools.setup(
     url="https://github.com/ixaustralia/pyexos",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 3 - Alpha" "Programming Language :: Python :: 3",
-        "License :: GPL version 3",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires="~=3.5",
+    install_requires=["netmiko"],
 )
