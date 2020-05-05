07c2e75e2cfc17793ca48db29a1afc7224565712
nicko170 <nick@npratley.net>
2020-05-06 09:17:51 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,14 +17,12 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import setuptools
 
-from pyexos import __version__
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pyexos-ixau",
-    version=__version__,
+    version='0.0.1',
     author="Nick Pratley",
     author_email="nick@ix.asn.au",
     description="pyexos - An Extreme Networks config manipulation tool",
